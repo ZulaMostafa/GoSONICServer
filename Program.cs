c74bfcb6b2d486af66edbe846cd62063224880f2
Salah Mostafa <33065229+ZulaMostafa@users.noreply.github.com>
2019-12-29 23:13:45 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoSONICServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
